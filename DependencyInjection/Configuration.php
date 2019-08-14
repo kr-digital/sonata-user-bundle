43222f90b97b1979ae898295d508cd85257f27ca
samovar <prototype.denis@gmail.com>
2019-08-14 19:27:40 +0300

    sf4

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -20,7 +20,6 @@ namespace KR\SonataUserBundle\DependencyInjection;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
-
 /**
  * @author KR Digital <box@kr.digital>
  */
@@ -31,14 +30,11 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder(): TreeBuilder
     {
-        $treeBuilder = new TreeBuilder();
+        $treeBuilder = new TreeBuilder('kr_sonata_user');
 
-        // Keep compatibility with symfony/config < 4.2
-        if (!\method_exists($treeBuilder, 'getRootNode')) {
-            $rootNode = $treeBuilder->root('kr_sonata_user');
-        } else {
-            $rootNode = $treeBuilder->getRootNode();
-        }
+        $rootNode = \method_exists($treeBuilder, 'getRootNode')
+            ? $treeBuilder->getRootNode()
+            : $treeBuilder->root('kr_sonata_user');
 
         $rootNode
             ->children()
